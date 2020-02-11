74aeabea17825768f5191daed612f1105dcc0333
piggy-devil <muupaza@gmail.com>
2020-02-11 14:09:17 +0700

    Retrieving friendships

--- app/Friend.php
+++ app/Friend.php
@@ -7,4 +7,18 @@ use App\Model;
 class Friend extends Model
 {
     protected $dates = ['confirmed_at'];
+
+    public static function friendship($userId)
+    {
+        return (new static())
+            ->where(function ($query) use ($userId) {
+                return $query->where('user_id', auth()->user()->id)
+                    ->where('friend_id', $userId);
+            })
+            ->orWhere(function ($query) use ($userId) {
+                return $query->where('friend_id', auth()->user()->id)
+                    ->where('user_id', $userId);
+            })
+            ->first();
+    }
 }
